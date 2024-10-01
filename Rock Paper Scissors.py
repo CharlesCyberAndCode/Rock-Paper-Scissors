63c0ac7bf4395f2ca410f741fd9c3b251619c416
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-10-01 17:50:07 -0400

    Add files via upload
    Made UX nicer to look at and interact with. Added a wins and losses counter that you can look at while playing to see how you're doing!

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,5 +1,7 @@
 #### Rock Paper Scissors
 import random  
+wins = 0
+losses = 0
 print("Hello. Welocome to Rock Paper Scissors!")
 print("")
 while True:
@@ -10,53 +12,48 @@ while True:
         userInput = input("")
         userInput = userInput.lower()
         if userInput == "rock" and generatedNum == 2:
-            print("You chose Rock!")
-            print("And I chose...")
-            print("Scissors! You win!")
+            print("You chose Rock! " "And I chose... " "Scissors! You win!")
+            wins = wins + 1
             break
         elif userInput == "paper" and generatedNum == 0:
-            print("You chose Paper!")
-            print("And I chose...")
-            print("Rock! You win!")
+            print("You chose Paper! " "And I chose... " "Rock! You win!")
+            wins = wins + 1
             break
         elif userInput == "scissors" and generatedNum == 1:
-            print("You chose Scissors!")
-            print("And I chose...")
-            print("Paper! You win!")
+            print("You chose Scissors! " "And I chose... " "Paper! You win!")
+            wins = wins + 1
             break
         elif userInput == "scissors" and generatedNum == 2:
-            print("You chose Scissors!")
-            print("And I chose...")
-            print("Scissors! Hmm.. seems we've tied.")
+            print("You chose Scissors! " "And I chose... " "Scissors! Hmm.. seems we've tied.")
             break
         elif userInput == "scissors" and generatedNum == 0:
-            print("You chose Scissors!")
-            print("And I chose...")
-            print("Rock! I win! Take that!")
+            print("You chose Scissors! " "And I chose... " "Rock! I win! Take that!")
+            losses = losses + 1
             break
         elif userInput == "paper" and generatedNum == 1:
-            print("You chose Paper!")
-            print("And I chose...")
-            print("Paper! Hmm.. seems we've tied.")
+            print("You chose Paper! " "And I chose... " "Paper! Hmm.. seems we've tied.")
             break
         elif userInput == "paper" and generatedNum == 2:
-            print("You chose Paper!")
-            print("And I chose...")
-            print("Scissors! I win! Take that!")
+            print("You chose Paper! " "And I chose... " "Scissors! I win! Take that!")
+            losses = losses + 1
             break
         elif userInput == "rock" and generatedNum == 0:
-            print("You chose Rock!")
-            print("And I chose...")
-            print("Rock! Hmm.. seems we've tied.")
+            print("You chose Rock! " "And I chose... " "Rock! Hmm.. seems we've tied.")
             break
         elif userInput == "rock" and generatedNum == 1:
-            print("You chose Rock!")
-            print("And I chose...")
-            print("Paper! I win! Take that!")
+            print("You chose Rock! " "And I chose... " "Paper! I win! Take that!")
+            losses = losses + 1
             break
         else:
             continue
-    print("")       
-    print("Good game. Let's play again!")
-    print("Hit enter to play again!") 
-    input("")
+    print("Good game!")
+    print("")
+    print("Would you like to see how many times you've won and lost against me?")
+    print("Type yes to see history. Hit enter to skip and play again.")
+    history = input("")
+    history = history.lower()
+    if history == "yes":
+        print(f"You have won against me {wins} times.")
+        print(f"You have lost against me {losses} times.") 
+        print("")
+    
